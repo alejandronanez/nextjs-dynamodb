ac710f90e07b24bda6ae2e71becb845630c04038
Alejandro Nanez <alejonanez@gmail.com>
2021-06-25 23:33:12 -0500

    CRUD using Api routes

--- pages/api/item.js
+++ pages/api/item.js
@@ -2,13 +2,55 @@ import * as uuid from 'uuid';
 import dynamoDb from '../../lib/dynamo';
 
 export default async function handler(req, res) {
-  const item = {
-    id: uuid.v4(),
-    content: 'test',
-    createdAt: Date.now()
+  if (req.method === 'PUT') {
+    /**
+     * Create items with:
+     * curl -X PUT http://localhost:3000/api/item -d '{"content": "testing this again"}' -H "Content-type: application/json"
+     */
+    const item = {
+      id: uuid.v4(),
+      content: req.body.content,
+      createdAt: Date.now()
+    }
+
+    await dynamoDb.put({Item: item});
+
+    res.status(201).json(item)
   }
 
-  await dynamoDb.put({Item: item});
+  if (req.method === 'GET') {
+    const {Item} = await dynamoDb.get({
+      Key: {
+        id: req.query.id
+      }
+    })
 
-  res.status(201).json(item)
+    res.status(200).json(Item)
+  }
+
+  if (req.method === 'POST') {
+    const {Attributes} = await dynamoDb.update({
+      Key: {
+        id: req.body.id,
+      },
+      UpdateExpression: 'SET content = :content',
+      ExpressionAttributeValues: {
+        ':content': req.body.content || null
+      },
+      Return: 'ALL_NEW'
+    })
+
+    res.status(200).json(Attributes)
+  }
+
+  if (req.method === 'DELETE') {
+    await dynamoDb.delete({
+      Key: {
+        id: req.body.id,
+      },
+    })
+
+    res.status(204).json({})
+  }
+
 }
