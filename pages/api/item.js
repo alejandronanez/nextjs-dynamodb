38b7a56d3a256fd07cb42eaea31374ec21158ed7
Alejandro Nanez <alejonanez@gmail.com>
2021-06-25 23:21:09 -0500

    Created a new item in DynamoDb using API routes

--- pages/api/item.js
+++ pages/api/item.js
@@ -1,5 +1,14 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    name: 'Alejandro'
-  })
+import * as uuid from 'uuid';
+import dynamoDb from '../../lib/dynamo';
+
+export default async function handler(req, res) {
+  const item = {
+    id: uuid.v4(),
+    content: 'test',
+    createdAt: Date.now()
+  }
+
+  await dynamoDb.put({Item: item});
+
+  res.status(201).json(item)
 }
