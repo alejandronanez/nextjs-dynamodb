1b5979cc6161a88d65570bab388321a44cb19632
Alejandro Nanez <alejonanez@gmail.com>
2021-06-25 23:36:58 -0500

    Fetching data from DynamoDB using SWR

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then(res => res.json())
+
 export default function Index() {
-  return 'Hello world'
+  const {data, error} = useSWR('/api/item?id=22f61264-5a07-4dae-ade5-b7a06e590a08', fetcher);
+
+  if (error) {
+    return <div>Failed to load</div>
+  }
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+
+  return <code>{JSON.stringify(data, null, 2)}</code>
 }
